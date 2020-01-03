61541da7936b152e63bef4fa9016b73004a2726b
juedui0769 <chixin0769@163.com>
2020-01-03 23:48:16 +0800

    defer

--- geekbang/ch10/func/func_test.go
+++ geekbang/ch10/func/func_test.go
@@ -47,3 +47,12 @@ func TestFn3(t *testing.T) {
 	t.Log(Sum(1, 2, 3))
 	t.Log(Sum(2, 3, 5, 8))
 }
+
+func Clear() {
+	fmt.Println("Clear resources.")
+}
+
+func TestDefer(t *testing.T){
+	defer Clear()
+	fmt.Println("Start")
+}
