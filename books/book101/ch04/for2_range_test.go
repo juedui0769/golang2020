fe2c309a7b92887752d0460271edbacb30959d09
juedui0769 <chixin0769@163.com>
2020-01-11 21:11:48 +0800

    2020-01-11 : for and range

--- books/book101/ch04/for2_range_test.go
+++ books/book101/ch04/for2_range_test.go
@@ -12,3 +12,52 @@ func TestForRange01(t *testing.T) {
 
 	}
 }
+
+// `namesCount` 包含了某个网站的所有用户昵称及其重复次数
+// 现在我们想从中查找到所有的只包含中文的用户昵称的计数信息。
+func TestForAndRange(t *testing.T) {
+	var namesCount map[string]int
+	targetsCount := make(map[string]int)
+	for k, v := range namesCount {
+		matched := true
+		for _, r := range k {
+			if r < '\u4e00' || r > '\u9fbf' {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			targetsCount[k] = v
+		}
+	}
+}
+
+// [改写]改写上面的例子
+//  找到第一个非全中文的用户昵称，从内层循环退出，并从外层循环退出
+//
+func TestForAndRange02(t *testing.T) {
+	var namesCount map[string]int
+	targetsCount := make(map[string]int)
+	for k, v := range namesCount {
+		matched := true
+		for _, r := range k {
+			if r < '\u4e00' || r > '\u9fbf' {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			targetsCount[k] = v
+		} else {
+			break
+		}
+	}
+}
+
+// `break` 后面可以放置标签，代表跳转到的位置。
+
+
+
+
+
+
