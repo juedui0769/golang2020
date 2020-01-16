6bca8d62e098b1ea814d90a64082b0a933015d10
juedui0769 <chixin0769@163.com>
2020-01-16 12:53:39 +0800

    20200116 : Key 的类型

--- books/book101/basic/map/order_map.go
+++ books/book101/basic/map/order_map.go
@@ -1,5 +1,10 @@
 package _map
 
+import (
+	"reflect"
+	"sort"
+)
+
 type OrderedMap struct {
 	keys []interface{}
 	m map[interface{}]interface{}
@@ -15,6 +20,28 @@ func (omap *OrderedMap) Less(i, j int) bool {
 
 func (omap *OrderedMap) Swap(i, j int) {
 	omap.keys[i], omap.keys[j] = omap.keys[j], omap.keys[i]
+}
+
+type Keys interface {
+	// 在Keys接口类型中嵌入sort.Interface接口类型，就意味着Keys类型的值一定是可排序的。
+	sort.Interface
+	Add(k interface{}) bool
+	Remove(k interface{}) bool
+	Clear()
+	Get(index int) interface{}
+	GetAll() []interface{}
+	Search(k interface{}) (index int, contains bool)
+	ElemType() reflect.Type
+	// 通过把比较两个元素值大小的问题抛给使用者，
+	// 我们既解决了需要动态确定元素类型的问题，
+	// 又明确了比较两个元素值大小的解决方式。
+	CompareFunc() func(interface{}, interface{}) int8
+}
+
+type myKeys struct {
+	container []interface{}
+	compareFunc func(interface{}, interface{}) int8
+	elemType reflect.Type
 }
 
 
