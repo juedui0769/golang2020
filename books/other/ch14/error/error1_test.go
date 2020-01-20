ff6516f8e5edc5af74d8ce74043ea1b1c14a2e09
juedui0769 <chixin0769@163.com>
2020-01-20 11:37:59 +0800

    fib , 增加错误处理

--- books/other/ch14/error/error1_test.go
+++ books/other/ch14/error/error1_test.go
@@ -1,19 +1,30 @@
 package error
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-func GetFibonacci(n int) []int {
+func GetFibonacci(n int) ([]int, error) {
+	if n < 2 || n > 100 {
+		return nil, errors.New("n should be in [2, 100]")
+	}
 	fibList := []int{1, 1}
 	for i := 2; i < n; i++ {
 		fibList = append(fibList, fibList[i-2]+fibList[i-1])
 	}
-	return fibList
+	return fibList, nil
 }
 
 func TestGetFibonacci(t *testing.T) {
-	t.Log(GetFibonacci(10))
+	if v, err := GetFibonacci(-10); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(v)
+	}
+	if v, err := GetFibonacci(10); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(v)
+	}
 }
-
-
-
-
