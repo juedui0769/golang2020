a724344f2ca09b9e8561fb14cffdc7deb9ba0d01
juedui0769 <chixin0769@163.com>
2020-01-20 10:47:36 +0800

    20200120 : 极客时间Golang课程，多态

---
+++ books/other/ReadMe.md
@@ -0,0 +1,10 @@
+
+<time.geekbang.org>
+
+极客时间上的golang视频课程
+
+-----
+
+
+
+
---
+++ books/other/polymorphic/polymorphic1_test.go
@@ -0,0 +1,33 @@
+package polymorphic
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Code string
+
+type Programmer interface {
+	WriteHelloWorld() Code
+}
+
+type GoProgrammer struct {}
+
+func (p *GoProgrammer) WriteHelloWorld() Code {
+	return "fmt.Println(\"Hello World!\")"
+}
+
+type JavaProgrammer struct {}
+
+func (p *JavaProgrammer) WriteHelloWorld() Code {
+	return "System.out.println(\"Hello World!\")"
+}
+
+func writeFirstProgram(p Programmer) {
+	fmt.Printf("%T %v\n", p, p.WriteHelloWorld())
+}
+
+func TestPolymorphic01(t *testing.T) {
+
+}
+
