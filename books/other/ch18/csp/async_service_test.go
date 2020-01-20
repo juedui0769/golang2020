6138e5ba8c020346e508375d947f2fed69a822f4
juedui0769 <chixin0769@163.com>
2020-01-20 22:18:32 +0800

    2 CSP : Communicating sequential processes

--- books/other/ch18/csp/async_service_test.go
+++ books/other/ch18/csp/async_service_test.go
@@ -8,16 +8,37 @@ import (
 
 func service() string {
 	time.Sleep(time.Microsecond * 50)
-	return "Done"
+	return "1 Done"
 }
 
 func otherTask() {
-	fmt.Println("working on something else")
+	fmt.Println("2 working on something else")
 	time.Sleep(time.Microsecond * 100)
-	fmt.Println("Task is done.")
+	fmt.Println("2 Task is done.")
 }
 
 func TestService(t *testing.T) {
 	fmt.Println(service())
 	otherTask()
 }
+
+func AsyncService() chan string {
+	retCh := make(chan string)
+	go func() {
+		ret := service()
+		fmt.Println(">> returned result.")
+		retCh <- ret
+		fmt.Println(">> service exited.")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
+
+
+
