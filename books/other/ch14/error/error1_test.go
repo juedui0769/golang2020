0141b79215d77fbfd22c234c851bbc6da1c57939
juedui0769 <chixin0769@163.com>
2020-01-20 11:50:29 +0800

    让错误类型更具体

--- books/other/ch14/error/error1_test.go
+++ books/other/ch14/error/error1_test.go
@@ -5,9 +5,15 @@ import (
 	"testing"
 )
 
+var LessThanTwoError error = errors.New("n should be not less than 2")
+var LargerThanHundredError = errors.New("n should be not larger than 100")
+
 func GetFibonacci(n int) ([]int, error) {
-	if n < 2 || n > 100 {
-		return nil, errors.New("n should be in [2, 100]")
+	if n < 2 {
+		return nil, LessThanTwoError
+	}
+	if n > 100 {
+		return nil, LargerThanHundredError
 	}
 	fibList := []int{1, 1}
 	for i := 2; i < n; i++ {
@@ -18,12 +24,32 @@ func GetFibonacci(n int) ([]int, error) {
 
 func TestGetFibonacci(t *testing.T) {
 	if v, err := GetFibonacci(-10); err != nil {
-		t.Error(err)
+		if err == LessThanTwoError {
+			t.Error("Need a larger number")
+		}
+		if err == LargerThanHundredError {
+			t.Error("Need a small number")
+		}
 	} else {
 		t.Log(v)
 	}
 	if v, err := GetFibonacci(10); err != nil {
-		t.Error(err)
+		if err == LessThanTwoError {
+			t.Error("Need a larger number")
+		}
+		if err == LargerThanHundredError {
+			t.Error("Need a small number")
+		}
+	} else {
+		t.Log(v)
+	}
+	if v, err := GetFibonacci(101); err != nil {
+		if err == LessThanTwoError {
+			t.Error("Need a larger number")
+		}
+		if err == LargerThanHundredError {
+			t.Error("Need a small number")
+		}
 	} else {
 		t.Log(v)
 	}
