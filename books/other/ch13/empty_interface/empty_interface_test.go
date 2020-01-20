d66bcc020c67a405ab814324bc3afcc7e69dae5f
juedui0769 <chixin0769@163.com>
2020-01-20 11:16:44 +0800

    使用 switch 改写类型判断

--- books/other/ch13/empty_interface/empty_interface_test.go
+++ books/other/ch13/empty_interface/empty_interface_test.go
@@ -6,21 +6,29 @@ import (
 )
 
 func DoSomething(p interface{}) {
-	if i, ok := p.(int); ok {
-		fmt.Println("Integer", i)
-		return
-	}
-	if s, ok := p.(string); ok {
-		fmt.Println("String", s)
-		return
-	}
-	fmt.Println("Unkown Type")
+	//if i, ok := p.(int); ok {
+	//	fmt.Println("Integer", i)
+	//	return
+	//}
+	//if s, ok := p.(string); ok {
+	//	fmt.Println("String", s)
+	//	return
+	//}
+	//fmt.Println("Unkown Type")
 
 
 	// fmt.Println(p.(type))
 	// 上面的语句不能直接打印, 否则会报错
 	// Use of .(type) outside type switch
 
+	switch v := p.(type) {
+	case int:
+		fmt.Println("Integer", v)
+	case string:
+		fmt.Println("String", v)
+	default:
+		fmt.Println("Unkown Type")
+	}
 
 }
 
