b584d1d45ecb650136a9c1f4152df7c19df8d0f2
juedui0769 <chixin0769@163.com>
2020-01-20 21:43:18 +0800

    使用 sync.Mutex 保证线程安全

--- books/other/ch17/share_mem/share_mem_test.go
+++ books/other/ch17/share_mem/share_mem_test.go
@@ -1,11 +1,12 @@
 package share_mem
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
 
-
+// 线程不安全的
 func TestCounter(t *testing.T) {
 	counter := 0
 	for i := 0; i < 5000; i++ {
@@ -17,5 +18,21 @@ func TestCounter(t *testing.T) {
 	t.Logf("counter = %d", counter)
 }
 
+// 线程安全的
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
 
 
