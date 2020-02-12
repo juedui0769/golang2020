c26e3dcd263a60aca5716611d324fae105f49a2c
juedui0769 <chixin0769@163.com>
2020-02-12 18:16:11 +0800

    20200212 : web 编程

--- web/ReadMe.md
+++ web/ReadMe.md
@@ -1,4 +1,8 @@
 ### web
 
 - 《Go+Web编程+》.(谢孟军).
+- Github: <https://github.com/astaxie/build-web-application-with-golang/blob/master/zh/preface.md>
+- Gitee: <https://gitee.com/jazzme/build-web-application-with-golang/blob/master/zh/preface.md>
+    - Github 上看不到图片，还是去Gitee上阅读吧！
+
 
--- web/web/web_helloworld.go
+++ web/web/web_helloworld.go
@@ -6,15 +6,15 @@ import (
 	"strings"
 )
 
-func SayHelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // 解析参数，默认是不会解析的
-	fmt.Println(r.Form) // 这些信息时输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
+func SayHelloName(writer http.ResponseWriter, req *http.Request) {
+	req.ParseForm()       // 解析参数，默认是不会解析的
+	fmt.Println(req.Form) // 这些信息时输出到服务器端的打印信息
+	fmt.Println("path", req.URL.Path)
+	fmt.Println("scheme", req.URL.Scheme)
+	fmt.Println(req.Form["url_long"])
+	for k, v := range req.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello golang!") // 这个是写入到 w 的是输出到客户端的
+	fmt.Fprintf(writer, "Hello golang!") // 这个是写入到 writer 的是输出到客户端的
 }
