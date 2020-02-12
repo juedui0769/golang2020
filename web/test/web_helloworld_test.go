9b4951fc3540bf5140076dead8781a6e046566da
juedui0769 <chixin0769@163.com>
2020-02-12 14:58:46 +0800

    20200212 : golang http 样例

---
+++ web/test/web_helloworld_test.go
@@ -0,0 +1,19 @@
+package web_test
+
+import (
+	"testing"
+)
+
+// 可以测试一次请求
+// 但不要在这里启动服务
+// 应该在 main 方法中启动服务
+func TestWebHelloWorld(t *testing.T) {
+	//http.HandleFunc("/", web.SayHelloName)       // 设置访问的路由
+	//err := http.ListenAndServe(":9090", nil) // 设置监听的端口
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
+}
+
+
+
---
+++ web/web/web_helloworld.go
@@ -0,0 +1,20 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func SayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // 解析参数，默认是不会解析的
+	fmt.Println(r.Form) // 这些信息时输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello golang!") // 这个是写入到 w 的是输出到客户端的
+}
